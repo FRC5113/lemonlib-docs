f9b6b917a441b9705bd6ebb43a1f5f0e2cc93865
Ryanforce08 <rradtke1208@gmail.com>
2025-12-03 22:04:26 -0500

    update docs to use auro doc

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,66 +3,110 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: node({
-      mode: 'standalone',
-    }),
-    integrations: [
-        starlight({
-            title: 'LemonLib documentation',
-            favicon: '/lemons.ico',
-            social: [
-                { icon: 'github', label: 'GitHub', href: 'https://github.com/FRC5113/LemonLib' }, 
-                { icon: 'instagram', label: 'Instagram', href: "https://www.instagram.com/frc5113/"}
-            ],
-            sidebar: [
-                {
-                    label: 'Guides',
-                    items: [
-                        // Each item here is one entry in the navigation menu.
-                        { label: 'Install', slug: 'guides/install' },
-                    ],
-                },
-                {
-                    label: 'Reference',
-                    items: [
-                        {label: 'CTRE',
-                            items: [
-                            {label: "LemonPigeon", slug: 'reference/pigeon'},
-                            {label: "LemonTalonFX ", slug: 'reference/talonfx'}
-                            ]
-                        },
-                        { label: "Drive", items: [
-                            {label: "KilloughDrive (kiwi)", slug: 'reference/kiwi'}
-                        ]},
-                        {
-                            label: "lemonbot", items: [
-                                {label: "LemonRobot", slug: 'reference/lemonrobot'}
-                            ]
-                        },
-                        {
-                            label: "Sim", items: [
-                                {label: "FalconSim", slug: 'reference/falconsim'},
-                                {label: "KilloughSim (Kiwi sim)", slug: 'reference/kiwisim'},
-                                {label: "LemonCamSim", slug: 'reference/lemoncamsim'},
-                                {label: "LemonInputSim", slug: 'reference/lemoninputsim'}
-                            ]
-                        },
-                        {
-                            label: "Util", items: [
-                                {label: "AlertType", slug: 'reference/alerttype'},
-                                {label: "Alert", slug: 'reference/alert'},
-                                {label: "AlertManager", slug: 'reference/alertm'},
-                                {label: "LedController", slug: 'reference/ledcontroler'},
-                                {label: "misc", slug: "reference/misc"},
-                                {label: "sysid", slug: "reference/sysid"}
-                            ]
-                        }
-                    ]
-                },
-            ],
-        }),
-    ],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+  integrations: [
+    starlight({
+      title: 'LemonLib documentation',
+      favicon: '/lemons.ico',
+      social: [
+        { icon: 'github', label: 'GitHub', href: 'https://github.com/FRC5113/LemonLib' },
+        { icon: 'instagram', label: 'Instagram', href: 'https://www.instagram.com/frc5113/' }
+      ],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            { label: 'Install', slug: 'guides/install' }
+          ]
+        },
+        {
+          label: 'Reference',
+          items: [
+{
+  label: 'Ctre',
+  items: [
+    { label: 'LemonPigeon', slug: 'reference/ctre/lemonpigeon' },
+    { label: 'LemonTalonFX', slug: 'reference/ctre/lemontalonfx' }
+  ]
+},
+{
+  label: 'Drive',
+  items: [
+    { label: 'KilloughDrive', slug: 'reference/drive/killoughdrive' },
+    { label: 'Vector2d', slug: 'reference/drive/vector2d' },
+    { label: 'SwagDrive', slug: 'reference/drive/swagdrive' }
+  ]
+},
+{
+  label: 'Funnies',
+  items: [
+    { label: 'Funnycontrollers', slug: 'reference/funnies/funnycontrollers' }
+  ]
+},
+{
+  label: 'Grapple',
+  items: [
+    { label: 'LaserCAN', slug: 'reference/grapple/lasercan' },
+    { label: 'LaserCanMeasurement', slug: 'reference/grapple/lasercanmeasurement' },
+    { label: 'LaserCanRoi', slug: 'reference/grapple/lasercanroi' },
+    { label: 'MitoCANdria', slug: 'reference/grapple/mitocandria' }
+  ]
+},
+{
+  label: 'Lemonbot',
+  items: [
+    { label: 'LemonRobot', slug: 'reference/lemonbot/lemonrobot' },
+    { label: 'LemonComponent', slug: 'reference/lemonbot/lemoncomponent' }
+  ]
+},
+{
+  label: 'Root',
+  items: [
+    { label: 'LemonInput', slug: 'reference/root/lemoninput' },
+    { label: 'xbox_buttons', slug: 'reference/root/xbox-buttons' },
+    { label: 'ps5_buttons', slug: 'reference/root/ps5-buttons' },
+    { label: 'legion_buttons', slug: 'reference/root/legion-buttons' },
+    { label: 'LemonCamera', slug: 'reference/root/lemoncamera' }
+  ]
+},
+{
+  label: 'Simulation',
+  items: [
+    { label: 'LemonCameraSim', slug: 'reference/simulation/lemoncamerasim' },
+    { label: 'FalconSim', slug: 'reference/simulation/falconsim' },
+    { label: 'LemonInputSim', slug: 'reference/simulation/lemoninputsim' },
+    { label: 'KilloughDriveSim', slug: 'reference/simulation/killoughdrivesim' }
+  ]
+},
+{
+  label: 'Smart',
+  items: [
+    { label: 'SmartProfile', slug: 'reference/smart/smartprofile' },
+    { label: 'SmartController', slug: 'reference/smart/smartcontroller' },
+    { label: 'SmartPreference', slug: 'reference/smart/smartpreference' },
+    { label: 'SmartNT', slug: 'reference/smart/smartnt' }
+  ]
+},
+{
+  label: 'Util',
+  items: [
+    { label: 'AlertType', slug: 'reference/util/alerttype' },
+    { label: 'Alert', slug: 'reference/util/alert' },
+    { label: 'AlertManager', slug: 'reference/util/alertmanager' },
+    { label: 'OneWaySlewRateLimiter', slug: 'reference/util/onewayslewratelimiter' },
+    { label: 'NotificationLevel', slug: 'reference/util/notificationlevel' },
+    { label: 'Notification', slug: 'reference/util/notification' },
+    { label: 'MagicSysIdRoutine', slug: 'reference/util/magicsysidroutine' },
+    { label: 'LEDController', slug: 'reference/util/ledcontroller' }
+  ]
+}
+          ]
+        }
+      ]
+    })
+  ]
 });
